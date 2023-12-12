d6acae2fe96975132274b7e064f85ba6dbbdec41
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-12 22:32:59 +0530

    carousel done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import Footer from "./components/Footer/Footer";
+import Header from "./components/HomePage/Header";
 import Navbar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Footer />
+      <Header />
+      {/* <Footer /> */}
     </>
   );
 }
---
+++ src/components/HomePage/Header.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Image1 from "../../assets/carousel1.webp";
+import Image2 from "../../assets/carousel2.webp";
+import Image3 from "../../assets/carousel3.webp";
+import Image4 from "../../assets/carousel4.webp";
+
+const Header = () => {
+  const [image, setImage] = useState(0);
+  const images = [Image1, Image2, Image3, Image4];
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setImage((prev) => (prev + 1) % images.length);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [image]);
+  return (
+    <div>
+      <div className="w-full">
+        <img
+          src={images[image]}
+          alt={`Image ${image + 1}`}
+          className="w-screen h-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
