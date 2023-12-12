6f73b261e4e39560ae6afd60d1bb8661388385f1
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-12 13:11:41 +0530

    working on footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="p-25 text-amber-800 leading-5">
+        <div className="bg-slate-300 rounded-20">
+          <div className="py-30 border-t border-solid">
+            <div className="max-w-1500 mx-auto">hello</div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
