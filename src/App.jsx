57571138198c12a9c4515dd67dfdc6a499bcb93e
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-13 19:27:42 +0530

    working on filter section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Cards from "./components/Cards/Cards";
+import FilterSection from "./components/Cards/FilterSection";
 import Header from "./components/HomePage/Header";
 import Navbar from "./components/Navbar/Navbar";
 // import Footer from "./components/Footer/Footer";
@@ -8,7 +9,8 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
-      <Header />
+      {/* <Header /> */}
+      <FilterSection />
       {/* <Cards /> */}
       {/* <Footer /> */}
     </>
---
+++ src/components/Cards/FilterSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FilterSection = () => {
+  return (
+    <>
+      <aside className="w-[15rem] h-screen bg-white">hello</aside>
+    </>
+  );
+};
+
+export default FilterSection;
