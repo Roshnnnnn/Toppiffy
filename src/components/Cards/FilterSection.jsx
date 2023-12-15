8a971e0a8b8e889e341974354426c7afaee4e934
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-15 19:48:25 +0530

    done

--- src/components/Cards/FilterSection.jsx
+++ src/components/Cards/FilterSection.jsx
@@ -1,71 +1,29 @@
-import { useState } from "react";
+import React from "react";
 
-const ImageGallery = () => {
+const FilterSection = () => {
   return (
-    <div className="flex">
-      {/* Fixed filter section */}
-      <div className="fixed left-0 top-0 h-full w-[15rem] bg-gray-200 p-4">
-        {/* Brand filter */}
-        <div className="my-[3rem]">
-          <h2 className="text-lg font-bold mb-4 text-center">Brand</h2>
-          <div className="items-center m-auto ml-[3rem]">
-            <label className="flex items-center mb-2">
-              <input type="checkbox" className="mr-2" />
-              Whittakar's
-            </label>
-            <label className="flex items-center mb-2">
-              <input type="checkbox" className="mr-2" />
-              Brand 2
-            </label>
-            <label className="flex items-center mb-2">
-              <input type="checkbox" className="mr-2" />
-              Brand 3
-            </label>
-            <label className="flex items-center">
-              <input type="checkbox" className="mr-2" />
-              Brand 4
-            </label>
-          </div>
-        </div>
+    <div className="flex justify-between items-center p-4 bg-gray-200">
+      <select className="p-2 border border-gray-300 rounded">
+        <option value="">All Brands</option>
+        <option value="brand1">Brand 1</option>
+        <option value="brand2">Brand 2</option>
+        <option value="brand3">Brand 3</option>
+        <option value="brand3">Brand 4</option>
+      </select>
 
-        {/* Sorting options */}
-        <div>
-          <h2 className="text-lg font-bold mb-2 text-center">Sort By</h2>
-          <div className="items-center m-auto">
-            <h2 className="text-lg font-bold mb-2 text-center">Price</h2>
-            <div className="items-center m-auto ml-[3rem]">
-              <label className="flex items-center mb-2">
-                <input type="radio" className="mr-2" />
-                Low To High
-              </label>
-              <label className="flex items-center mb-2">
-                <input type="radio" className="mr-2" />
-                High to low
-              </label>
-            </div>
-          </div>
-          <div className="items-center m-auto">
-            <h2 className="text-lg font-bold mb-2 text-center">
-              Alphabetically
-            </h2>
-            <div className="items-center m-auto ml-[3rem]">
-              <label className="flex items-center mb-2">
-                <input type="radio" className="mr-2" />A to Z
-              </label>
-              <label className="flex items-center mb-2">
-                <input type="radio" className="mr-2" />Z to A
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
+      <select className="p-2 border border-gray-300 rounded">
+        <option value="">Price</option>
+        <option value="lowToHigh">Low to High</option>
+        <option value="highToLow">High to Low</option>
+      </select>
 
-      {/* Main content */}
-      <div className="ml-30rem p-4">
-        {/* Your existing image gallery content goes here */}
-      </div>
+      <select className="p-2 border border-gray-300 rounded">
+        <option value="">Alphabetical</option>
+        <option value="aToZ">A to Z</option>
+        <option value="zToA">Z to A</option>
+      </select>
     </div>
   );
 };
 
-export default ImageGallery;
+export default FilterSection;
