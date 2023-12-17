b915778dd36da305fb9541b9b9bb0546183e55cf
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-17 13:13:49 +0530

    signup done

--- src/components/Profile/Signup.jsx
+++ src/components/Profile/Signup.jsx
@@ -1,74 +1,74 @@
-// import { Link } from "react-router-dom";
-// import { BiUser } from "react-icons/bi";
-// import { AiOutlineUnlock } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { BiUser } from "react-icons/bi";
+import { AiOutlineUnlock } from "react-icons/ai";
 
-// const Signup = () => {
-//   return (
-//     <div>
-//       {" "}
-//       <div
-//         className="bg-cover bg-center h-screen flex justify-center items-center"
-//         style={{
-//           backgroundImage:
-//             'url("https://images.pexels.com/photos/7525164/pexels-photo-7525164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")',
-//         }}
-//       >
-//         <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
-//           <div>
-//             <h1 className="text-4xl font-bold text-white mb-6 text-center">
-//               Sign Up
-//             </h1>
-//             <form action="" className="flex flex-col">
-//               <div className="my-4 relative">
-//                 <input
-//                   type="email"
-//                   className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
-//                   placeholder=""
-//                 />
-//                 <label className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
-//                   Your Email
-//                 </label>
-//                 <BiUser className="absolute top-0 right-4" />
-//               </div>
-//               <div className="my-4 relative">
-//                 <input
-//                   type="password"
-//                   placeholder=""
-//                   className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
-//                 />
-//                 <label className="absolute text-sm text-white duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
-//                   Password
-//                 </label>
-//                 <AiOutlineUnlock className="absolute top-0 right-4" />
-//               </div>{" "}
-//               <div className="my-4 relative">
-//                 <input
-//                   type="password"
-//                   placeholder=""
-//                   className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
-//                 />
-//                 <label className="absolute text-sm text-white duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
-//                   Confirm Password
-//                 </label>
-//                 <AiOutlineUnlock className="absolute top-0 right-4" />
-//               </div>
-//               <div className="flex justify-between items-center">
-//                 <Link to="/" className="text-blue-600">
-//                   Already Member
-//                 </Link>
-//               </div>
-//               <button
-//                 type="submit"
-//                 className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-amber-600 hover:text-white py-2 transition-colors duration-300"
-//               >
-//                 Sign Up
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const Signup = () => {
+  return (
+    <div>
+      {" "}
+      <div
+        className="bg-cover bg-center h-screen flex justify-center items-center"
+        style={{
+          backgroundImage:
+            'url("https://images.pexels.com/photos/7525164/pexels-photo-7525164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")',
+        }}
+      >
+        <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+          <div>
+            <h1 className="text-4xl font-bold text-white mb-6 text-center">
+              Sign Up
+            </h1>
+            <form action="" className="flex flex-col">
+              <div className="my-4 relative">
+                <input
+                  type="email"
+                  className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                  placeholder=""
+                />
+                <label className="absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
+                  Your Email
+                </label>
+                <BiUser className="absolute top-0 right-4" />
+              </div>
+              <div className="my-4 relative">
+                <input
+                  type="password"
+                  placeholder=""
+                  className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                />
+                <label className="absolute text-sm text-white duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
+                  Password
+                </label>
+                <AiOutlineUnlock className="absolute top-0 right-4" />
+              </div>{" "}
+              <div className="my-4 relative">
+                <input
+                  type="password"
+                  placeholder=""
+                  className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer"
+                />
+                <label className="absolute text-sm text-white duration-300 transform -translate-y-8 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-4 peer-focus:scale-75 peer-focus:-translate-y-6">
+                  Confirm Password
+                </label>
+                <AiOutlineUnlock className="absolute top-0 right-4" />
+              </div>
+              <div className="flex justify-between items-center">
+                <Link to="/" className="text-blue-600">
+                  Already Member
+                </Link>
+              </div>
+              <button
+                type="submit"
+                className="w-full mb-4 text-[18px] mt-6 rounded-full bg-white text-emerald-800 hover:bg-amber-600 hover:text-white py-2 transition-colors duration-300"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Signup;
+export default Signup;
