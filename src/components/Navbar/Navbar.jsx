73bc3ca0986be39c8e20de28bbda7f34e439ef38
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-22 03:01:00 +0530

    searchingon nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
     { name: "HOME", link: "/" },
     { name: "PRODUCT", link: "/product" },
     { name: "CONTACT", link: "/contact" },
-    { name: <FaSearch />, link: "/search" },
+    // { name: <FaSearch />, link: "/search" },
   ];
   let [open, setOpen] = useState(false);
   let [drop, setDrop] = useState(false);
@@ -56,7 +56,7 @@ const Nav = () => {
           <FaHamburger name={open ? "close" : "menu"} />
         </div>
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static cursor-pointer text-amber-600  bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-200 ease-in-out ${
             open ? "top-20" : "top-[-490px]"
           }`}
         >
@@ -75,6 +75,9 @@ const Nav = () => {
               </NavLink>
             </li>
           ))}
+          <li className="md:ml-8 text-xl md:my-0 my-7 mx-8 ">
+            <FaSearch />
+          </li>
           <li className="md:ml-8 text-xl md:my-0 my-7 mx-8">
             <NavLink
               to="/cart"
@@ -87,50 +90,16 @@ const Nav = () => {
               <FaShoppingBag />
             </NavLink>
           </li>
-          {/* <li className="md:ml-8 text-xl md:my-0 my-7 mx-8 justify-center cursor-pointer">
-            <button
-              className=" text-amber-600 font-semibold rounded inline-flex items-center"
-              onClick={auth.currentUser ? handleLogout : handleLogin}
-            >
-              <span className="mr-1">
-                <FaRegUser />
-              </span>
-            </button>
-            <div className="absolute z-10 hidden right-0 mt-2 w-36 bg-white rounded-md shadow-lg">
-              {auth.currentUser ? (
-                <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left">
-                  Logout
-                </button>
-              ) : (
-                <button className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left">
-                  Login
-                </button>
-              )}
-            </div>
-          </li> */}
           <li className="md:ml-8 text-xl md:my-0 my-7 mx-8 justify-center cursor-pointer">
             <button
               className="text-amber-600 font-semibold rounded inline-flex items-center"
-              onClick={() => setDrop(!drop)} // Toggle dropdown on click
+              onClick={() => setDrop(!drop)}
             >
               <span className="mr-1">
                 <FaRegUser />
               </span>
-              {drop && (
-                <svg
-                  className="ml-2 -mr-1 h-4 w-4 rotate-180"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              )}
             </button>
-            {drop && ( // Show dropdown content when open
+            {drop && (
               <div className="absolute z-10 mt-2 right-0 w-36 bg-white rounded-md shadow-lg">
                 {auth.currentUser ? (
                   <button
