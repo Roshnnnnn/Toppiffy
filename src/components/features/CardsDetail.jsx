593bdbe6aad249206fefa36b4e4775a775038be0
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-22 11:02:38 +0530

    UI update
    card Details

--- src/components/features/CardsDetail.jsx
+++ src/components/features/CardsDetail.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const CardsDetail = ({ item }) => {
   return (
     <div className="flex justify-center items-center my-12">
-      <div className="p-8 ">
-        <Link to="/product-details/:id">
-          <div className="w-[12rem] h-[16rem] ">
+      <div className="p-4 md:p-8 lg:p-12">
+        <Link to={`/product-details/${item.id}`}>
+          <div className="w-full lg:w-[12rem] md:h-[16rem] lg:h-[20rem]">
             <img
               src={item.images.image}
               alt=""
-              className="object-contain w-full h-full"
+              className="object-cover w-full h-full rounded-md"
             />
           </div>
         </Link>
-        <div className="flex flex-col justify-center">
+        <div className="flex flex-col justify-center mt-4">
           <span className="text-sm mb-2 text-center">{item.name}</span>
           <span className="text-lg text-center">${item.price}</span>
         </div>
