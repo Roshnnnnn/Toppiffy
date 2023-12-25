4c1ed66198b9774d161da652908a041b5b660143
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2023-12-26 00:07:10 +0530

    slider
    slider added in header

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,8 +1,3 @@
-import { useState, useEffect } from "react";
-import Image1 from "../../assets/carousel1.webp";
-import Image2 from "../../assets/carousel2.webp";
-import Image3 from "../../assets/carousel3.webp";
-import Image4 from "../../assets/carousel4.webp";
 import Card from "../features/Cards";
 import Image5 from "../../assets/hershey.webp";
 import Image6 from "../../assets/Nestle.webp";
@@ -14,36 +9,23 @@ import Footer from "../Footer/Footer";
 import Slider from "./Slider";
 
 const Header = () => {
-  const [image, setImage] = useState(0);
-  const images = [Image1, Image2, Image3, Image4];
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setImage((prev) => (prev + 1) % images.length);
-    }, 2000);
-    return () => clearTimeout(timeout);
-  }, [image]);
-
   return (
     <div>
-      <Navbar />
-      <Slider />
-      {/* <div className="w-full">
-        <img
-          src={images[image]}
-          alt={`Image ${image + 1}`}
-          className="w-full lg:h-[40rem] object-contain md:object-cover md:w-screen md:h-[25rem] sm:h-[19rem]"
-        />
-      </div> */}
-      {/* <div className="text-4xl justify-center text-center m-12">
+      <div className="sticky top-0 z-50">
+        <Navbar />
+      </div>
+      <div>
+        <Slider />
+      </div>
+      <div className="text-4xl justify-center text-center m-12">
         <div className="my-8">Top Brands</div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <Card image={Image5} />
           <Card image={Image6} />
           <Card image={Image7} />
         </div>
-      </div> */}
-      {/* <div className="text-4xl justify-center text-center m-8">
+      </div>
+      <div className="text-4xl justify-center text-center m-8">
         <h2 className="my-8">Best Sellers</h2>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {data.slice(0, 3).map((item, index) => (
@@ -52,8 +34,8 @@ const Header = () => {
             </div>
           ))}
         </div>
-      </div> */}
-      {/* <Footer /> */}
+      </div>
+      <Footer />
     </div>
   );
 };
