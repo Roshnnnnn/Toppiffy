7a97d1d14a74abf23cc7ec67b6fd1c9452e31fd5
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-07 21:00:12 +0530

    update productList

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -2,27 +2,37 @@ import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import data from "../../data.json";
 import CardsDetail from "../features/CardsDetail";
+import { Helmet } from "react-helmet-async";
 
 const ProductList = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <div className="flex-grow mt-2">
-        <div className="flex justify-center items-center">
-          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {data.map((item, index) => (
-              <div
-                key={index}
-                className="flex justify-center items-center my-0"
-              >
-                <CardsDetail item={item} brand={item.brand} id={item.id} />
-              </div>
-            ))}
+    <>
+      <Helmet>
+        <title>Products - ChocoKart</title>
+        <meta
+          name="description"
+          content="Browse our variety of toffees and chocolates. Find your favorite treats and enjoy!"
+        />
+      </Helmet>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow mt-2">
+          <div className="flex justify-center items-center">
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {data.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex justify-center items-center my-0"
+                >
+                  <CardsDetail item={item} brand={item.brand} id={item.id} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
