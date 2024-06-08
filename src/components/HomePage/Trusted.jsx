57a41a2278533a4a70671a7839bfa9577d88f501
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-09 01:16:31 +0530

    create trusted

---
+++ src/components/HomePage/Trusted.jsx
@@ -0,0 +1,50 @@
+const Trusted = () => {
+  return (
+    <section className="py-16 bg-gray-100 mx-[3rem]">
+      <div className="container mx-auto">
+        <h3 className="text-center text-2xl font-bold capitalize text-gray-800 mb-8">
+          Trusted By 1000+ Companies
+        </h3>
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+          <div className="flex justify-center items-center">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+              alt="trusted-brands"
+              className="min-w-[6rem] md:min-w-24 h-[6rem] md:h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+              alt="trusted-brands"
+              className="min-w-[6rem] md:min-w-24 h-[6rem] md:h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+              alt="trusted-brands"
+              className="min-w-[6rem] md:min-w-24 h-[6rem] md:h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+              alt="trusted-brands"
+              className="min-w-[6rem] md:min-w-24 h-[6rem] md:h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+              alt="trusted-brands"
+              className="min-w-[6rem] md:min-w-24 h-[6rem] md:h-24"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Trusted;
