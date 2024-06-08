2ce31e7e7483de7972cd7461a833f0168722942f
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-09 01:16:22 +0530

    update productSlice

--- src/components/redux/slices/productSlice.js
+++ src/components/redux/slices/productSlice.js
@@ -1,10 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { chocolates } from "../../../data.jsx";
+import { get, getDatabase, ref, set } from "firebase/database";
+import app from "../../config/firebase.js";
+
+const db = getDatabase(app);
+
+export const uploadJsonData = async () => {
+  try {
+    await set(ref(db, "products/"), chocolates);
+    console.log("Attempting to upload data to Firebase...");
+    console.log("Data successfully uploaded to Firebase");
+  } catch (error) {
+    console.error("Error uploading data to Firebase", error);
+  }
+};
+
+uploadJsonData();
+
+export const getJsonData = async () => {
+  const productRef = ref(db, "products");
+  try {
+    const snapshot = await get(productRef);
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      console.log("Tension ni lena ka data aa gya h", data);
+      return data;
+    } else {
+      console.log("Daya Kuch to gadbad hai");
+      return null;
+    }
+  } catch (error) {
+    console.log("Error getting data from firebase", error);
+  }
+};
+
+getJsonData();
 
 const initialState = {
   filteredChocolate:
     JSON.parse(sessionStorage.getItem("filteredData")) || chocolates,
   singleProduct: JSON.parse(sessionStorage.getItem("singleProduct")) || null,
+  products: [],
+  loading: false,
   error: false,
 };
 
@@ -21,9 +58,10 @@ const productSlice = createSlice({
         state.error = false;
         const saveState = JSON.stringify(filter);
         sessionStorage.setItem("filteredData", saveState);
-        console.log("filter", filter);
+        console.log("Filtered chocolates:", filter);
       } catch (error) {
         state.error = true;
+        console.error("Error filtering chocolates", error);
       }
     },
     singleProduct: (state, action) => {
@@ -34,9 +72,10 @@ const productSlice = createSlice({
         state.singleProduct = oneProduct;
         const saveState = JSON.stringify(oneProduct);
         sessionStorage.setItem("singleProduct", saveState);
-        console.log("oneProduct", oneProduct);
+        console.log("Single product:", oneProduct);
       } catch (error) {
         state.error = true;
+        console.error("Error setting single product", error);
       }
     },
   },
