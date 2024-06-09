57bd3f83e2e1c236ce5f0d28ddaa9722ea18f9de
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-09 13:36:39 +0530

    update productDetails

--- src/components/Product/ProductDetails.jsx
+++ src/components/Product/ProductDetails.jsx
@@ -3,7 +3,6 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import Accordion from "../features/Accordion";
 import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
 import Button from "../features/Button";
 import { Helmet } from "react-helmet-async";
 import { singleProduct } from "../redux/slices/productSlice";
@@ -33,67 +32,55 @@ const ProductDetails = () => {
       </Helmet>
       <div className="min-h-screen">
         <Navbar />
-        <div className="container m-auto py-8">
-          <div className="flex flex-col lg:flex-row p-4 gap-16 lg:items-center bg-white rounded-lg shadow-md">
-            <div className="flex flex-col gap-6 lg:w-2/4">
+        <div className="container mx-auto py-8 px-4 lg:px-8 ">
+          <div className="flex flex-col lg:flex-row gap-4 lg:items-start bg-white rounded-lg shadow-md">
+            <div className="lg:w-1/2">
               <img
                 src={product.images.image}
                 alt={product.name}
-                className="rounded-lg shadow-md"
+                className="rounded-lg shadow-md w-full"
               />
             </div>
-            <div className="flex flex-col gap-4 lg:w-2/4 items-center">
-              <div>
-                <h1 className="text-amber-700 font-semibold text-3xl lg:text-5xl">
-                  {product.name}
-                </h1>
-              </div>
-              <div>
-                <p className="font-semibold text-lg">{product.description}</p>
-              </div>
-              <div className="flex flex-col lg:flex-row gap-8">
-                <div className="mb-4 lg:mb-0">
-                  <Accordion title="Ingredients">
-                    <ul className="list-none gap-2">
-                      {product.ingredients.map((ingredient, i) => (
-                        <li key={i} className="text-sm">
-                          {ingredient}
-                        </li>
-                      ))}
-                    </ul>
-                  </Accordion>
-                </div>
-                <div className="mb-4 lg:mb-0">
-                  <Accordion title="Nutrition">
-                    <ul className="list-none gap-2">
-                      <li className="text-sm">
-                        Calories: {product.nutrition.calories}
+            <div className="lg:w-1/2 p-4 lg:p-[6rem]">
+              <h1 className="text-3xl lg:text-4xl font-semibold text-amber-700 mb-2">
+                {product.name}
+              </h1>
+              <p className="text-lg mb-4">{product.description}</p>
+              <div className="flex flex-col md:flex-row gap-4 mb-4">
+                <Accordion title="Ingredients">
+                  <ul className="list-none gap-2 ">
+                    {product.ingredients.map((ingredient, i) => (
+                      <li key={i} className="text-sm">
+                        {ingredient}
                       </li>
-                      <li className="text-sm">Fat: {product.nutrition.fat}g</li>
-                      <li className="text-sm">
-                        Carbohydrates: {product.nutrition.carbohydrates}g
-                      </li>
-                      <li className="text-sm">
-                        Protein: {product.nutrition.protein}g
-                      </li>
-                    </ul>
-                  </Accordion>
-                </div>
+                    ))}
+                  </ul>
+                </Accordion>
+                <Accordion title="Nutrition">
+                  <ul className="list-none gap-2 ">
+                    <li className="text-sm">
+                      Calories: {product.nutrition.calories}
+                    </li>
+                    <li className="text-sm">Fat: {product.nutrition.fat}g</li>
+                    <li className="text-sm">
+                      Carbohydrates: {product.nutrition.carbohydrates}g
+                    </li>
+                    <li className="text-sm">
+                      Protein: {product.nutrition.protein}g
+                    </li>
+                  </ul>
+                </Accordion>
               </div>
-              <div>
-                <div className="text-lg">Rating: {product.rating} ⭐</div>
+              <div className="text-lg mb-2">Rating: {product.rating} ⭐</div>
+              <div className="text-2xl font-semibold mb-4">
+                $ {product.price}
               </div>
-              <div className="text-2xl">$ {product.price}</div>
-
-              <div>
-                <div className="mt-6">
-                  <Button product={product} />
-                </div>
+              <div className="mt-6">
+                <Button product={product} />
               </div>
             </div>
           </div>
         </div>
-        <Footer />
       </div>
     </>
   );
