a541eb46adf8254767dd64f245c1c39b34137323
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-09 13:35:47 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <ToastContainer />
       <Outlet />
+      <Footer />
     </>
   );
 }
