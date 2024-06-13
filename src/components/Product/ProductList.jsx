b82f39c21ab0678a2a644574593aa6d37e7c9409
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-13 10:59:09 +0530

    update ProductList

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -23,22 +23,25 @@ const ProductList = () => {
           content="Browse our variety of toffees and chocolates. Find your favorite treats and enjoy!"
         />
       </Helmet>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <div className="flex-grow mt-2">
-          <h1 className="text-4xl font-bold tracking-normal leading-none my-4 sm:mx-12">
-            All Products
-          </h1>
-          <div className="flex justify-center items-center">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {products.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex justify-center items-center my-0"
-                >
-                  <CardsDetail item={item} brand={item.brand} id={item.id} />
-                </div>
-              ))}
+
+      <div>
+        <div className="flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow mt-2">
+            <h1 className="text-4xl font-bold tracking-normal leading-none my-4 sm:mx-12">
+              All Products
+            </h1>
+            <div className="flex justify-center items-center">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {products.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex justify-center items-center my-0"
+                  >
+                    <CardsDetail item={item} brand={item.brand} id={item.id} />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
