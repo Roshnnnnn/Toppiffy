a19b3d211ee3c7fc14d84d50bb327f48036fde97
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-13 10:59:01 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,41 @@ import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer/Footer";
+import { useEffect, useState } from "react";
+import { Watch } from "react-loader-spinner";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
   return (
     <>
-      <ToastContainer />
-      <Outlet />
-      <Footer />
+      {loading ? (
+        <div className="flex items-center justify-center min-h-screen">
+          <Watch
+            visible={true}
+            height="80"
+            width="80"
+            radius="48"
+            color="#924D3A"
+            ariaLabel="watch-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <>
+          <ToastContainer />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </>
   );
-}
+};
 
 export default App;
