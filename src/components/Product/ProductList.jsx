69d2740868f9aea790125c6fff16ba6cd3fb787e
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-13 12:48:28 +0530

    update ProductList

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,18 +1,43 @@
+import { useState, useEffect, useCallback } from "react";
 import Navbar from "../Navbar/Navbar";
 import CardsDetail from "../features/CardsDetail";
 import { Helmet } from "react-helmet-async";
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllProducts } from "../redux/slices/productSlice";
 
 const ProductList = () => {
   const dispatch = useDispatch();
-
   const { products } = useSelector((state) => state.chocolates);
 
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    const windowHeight = window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const remainingHeight = documentHeight - (windowHeight + scrollTop);
+
+    if (remainingHeight < 100 && !loading) {
+      setLoading(true);
+      setPage(page + 1);
+    }
+  }, [loading, page]);
+
   useEffect(() => {
-    dispatch(fetchAllProducts());
-  }, [dispatch]);
+    dispatch(fetchAllProducts(page));
+  }, [dispatch, page]);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [products]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll]);
 
   return (
     <>
@@ -24,25 +49,24 @@ const ProductList = () => {
         />
       </Helmet>
 
-      <div>
-        <div className="flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow mt-2">
-            <h1 className="text-4xl font-bold tracking-normal leading-none my-4 sm:mx-12">
-              All Products
-            </h1>
-            <div className="flex justify-center items-center">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {products.map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex justify-center items-center my-0"
-                  >
-                    <CardsDetail item={item} brand={item.brand} id={item.id} />
-                  </div>
-                ))}
-              </div>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <div className="flex-grow mt-2">
+          <h1 className="text-4xl font-bold tracking-normal leading-none my-4 sm:mx-12">
+            All Products
+          </h1>
+          <div className="flex justify-center items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {products.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex justify-center items-center my-0"
+                >
+                  <CardsDetail item={item} brand={item.brand} id={item.id} />
+                </div>
+              ))}
             </div>
+            {loading && <p>Loading...</p>}
           </div>
         </div>
       </div>
