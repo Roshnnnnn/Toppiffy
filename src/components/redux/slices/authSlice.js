e024e44a853f49d757c22cfc787ee6d8b0a381c7
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-13 12:48:46 +0530

    update AuthSlice

--- src/components/redux/slices/authSlice.js
+++ src/components/redux/slices/authSlice.js
@@ -1,40 +1,18 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   isLoggedIn: false,
-//   email: null,
-//   userId: null,
-// };
-
-// const authSlice = createSlice({
-//   name: "auth",
-//   initialState,
-//   reducers: {
-//     setActiveUser: (state, action) => {
-//       const { email, userId } = action.payload;
-//       state.isLoggedIn = true;
-//       state.email = email;
-//       state.userId = userId;
-//     },
-//     logoutUser: (state) => {
-//       state.isLoggedIn = false;
-//       state.email = null;
-//       state.userId = null;
-//     },
-//   },
-// });
-
-// export const { setActiveUser, logoutUser } = authSlice.actions;
-
-// export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
-// export const selectEmail = (state) => state.auth.email;
-// export const selectUserId = (state) => state.auth.userId;
-
-// export default authSlice.reducer;
-
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+const loadStateFromSessionStorage = () => {
+  try {
+    const serializedState = sessionStorage.getItem("authState");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const initialState = loadStateFromSessionStorage() || {
   isLoggedIn: false,
   email: null,
   userId: null,
@@ -51,12 +29,16 @@ const authSlice = createSlice({
       state.email = email;
       state.userId = userId;
       state.isSpecialMember = isSpecialMember;
+
+      sessionStorage.setItem("authState", JSON.stringify(state));
     },
     logoutUser: (state) => {
       state.isLoggedIn = false;
       state.email = null;
       state.userId = null;
       state.isSpecialMember = false;
+
+      sessionStorage.removeItem("authState");
     },
   },
 });
