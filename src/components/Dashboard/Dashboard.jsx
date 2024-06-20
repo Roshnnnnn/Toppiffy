eae06a554d9a34b41101c51cf7de20edf3ba9c61
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-06-20 23:04:13 +0530

    create dashboard

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,34 @@
+import Navbar from "../Navbar/Navbar";
+import Products from "./Products";
+import Users from "./Users";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="bg-gray-100 min-h-screen">
+        <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-semibold mb-4 text-center text-gray-800">
+            Dashboard
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="bg-white shadow-md rounded-lg p-6">
+              <h3 className="text-xl font-semibold mb-4 text-gray-800">
+                Users
+              </h3>
+              <Users />
+            </div>
+            <div className="bg-white shadow-md rounded-lg p-6">
+              <h3 className="text-xl font-semibold mb-4 text-gray-800">
+                Products
+              </h3>
+              <Products />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
